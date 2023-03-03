54327c0a3d54bd2732b4412e9167079bf4a13b60
Michele Moio <michele.moio@protonmail.com>
2023-03-03 12:57:29 +0100

    feat: CTAlias redirect

---
+++ src/app/src/api/ct-alias/controllers/ct-alias.ts
@@ -0,0 +1,7 @@
+/**
+ * ct-alias controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::ct-alias.ct-alias');
---
+++ src/app/src/api/ct-alias/services/ct-alias.ts
@@ -0,0 +1,7 @@
+/**
+ * ct-alias service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::ct-alias.ct-alias');
