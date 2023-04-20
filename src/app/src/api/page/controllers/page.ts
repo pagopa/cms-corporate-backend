8d077ce6ae8fc7c5c4e2acf250a031712f677639
Vito Falco <vitofalco@gmail.com>
2023-04-20 18:00:17 +0200

    Custom controller: added a "url" field

--- src/app/src/api/page/controllers/page.ts
+++ src/app/src/api/page/controllers/page.ts
@@ -4,4 +4,12 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::page.page');
+export default factories.createCoreController('api::page.page', 
+    () => ({
+    async findOne(ctx) {
+        const { data } = await super.findOne(ctx);
+        const url: string = data.attributes.url_path ? `${data.attributes.url_path}${data.attributes.slug}` : `/${data.attributes.slug}`;
+        data.attributes = {...data.attributes, url: url};
+        return data;
+    },
+}));
