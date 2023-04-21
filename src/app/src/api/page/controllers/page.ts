dec572a7200f5ed443a4f609d3255d53176ab217
Vito Falco <vitofalco@gmail.com>
2023-04-21 16:19:34 +0200

    Drop custom controller. Now we use a resolver in gatsby

--- src/app/src/api/page/controllers/page.ts
+++ src/app/src/api/page/controllers/page.ts
@@ -4,12 +4,4 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::page.page', 
-    () => ({
-    async findOne(ctx) {
-        const { data } = await super.findOne(ctx);
-        const url: string = data.attributes.url_path ? `${data.attributes.url_path}${data.attributes.slug}` : `/${data.attributes.slug}`;
-        data.attributes = {...data.attributes, url: url};
-        return data;
-    },
-}));
+export default factories.createCoreController('api::page.page')
